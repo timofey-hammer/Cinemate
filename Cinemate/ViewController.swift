c1ef921e6a938dcc1626d1f0e71306c632911c1f
Timofey Hammer <myservices.hammer@gmail.com>
2023-12-13 15:04:11 +0300

    Initial Commit

---
+++ Cinemate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinemate
+//
+//  Created by Timofey Hammer on 13.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
