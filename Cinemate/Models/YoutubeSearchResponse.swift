fc1890917b9d17897b3b1d8d8c7627c464e80b58
Timofey Hammer <myservices.hammer@gmail.com>
2023-12-20 14:21:34 +0300

    Open Movies

---
+++ Cinemate/Models/YoutubeSearchResponse.swift
@@ -0,0 +1,19 @@
+//
+//  YoutubeResponse.swift
+//  Cinemate
+//
+
+import Foundation
+
+struct YoutubeSearchResponse: Codable {
+    let items: [VideoElement]
+}
+
+struct VideoElement: Codable {
+    let id: IdVideoElement
+}
+
+struct IdVideoElement: Codable {
+    let kind: String
+    let videoId: String
+}
---
+++ Cinemate/ViewModels/PreviewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PreviewViewModel.swift
+//  Cinemate
+//
+//  Created by Timofey Hammer on 16.12.2023.
+//
+
+import Foundation
